934bbff0678b9aa5a78e5e975589ef22f98b9e06
HenriqueLefkum <hlefkum@gmail.com>
2023-05-12 12:24:34 -0300

    more components and add function

---
+++ src/components/CreateFuncionario/CreateFuncionario.js
@@ -0,0 +1,13 @@
+import './CreateFuncionarioStyle.css'
+
+function CreateFuncionario()
+{
+    return(
+        <div>
+            <h1>tomara que funcione</h1>
+            <input type='text' placeholder='Nome'></input>
+        </div>
+    );
+}
+
+export default CreateFuncionario;
--- src/pages/pagesFuncionario/FuncList/index.js
+++ src/pages/pagesFuncionario/FuncList/index.js
@@ -1,30 +1,90 @@
 //A intenção aqui é mostrar a lista dos funcionarios com as informações pertinentes
 //talvez mais pro futuro adicionar as opções do que mostrar e tambem para mandar pro excel ou imprimir em pdf {users.map((user) => (
-                        // <tr key={user.id}></tr>
+// <tr key={user.id}></tr>
+import React, { useState } from 'react'
+import {addDocs, getDocs , collections } from  'firebase/firestore'
+import CreateFuncionario from '../../../components/CreateFuncionario/CreateFuncionario';
+import ReactModal from 'react-modal';
 
 import './FuncListStyle.css'
 function FuncList()
 {
+    /*
+    const[nome,setNome] = useState("");
+    const[CPF, setCPF] = useState("");
+    const[Endereco,setEndereco] = useState("");
+    const[eNum, seteNum] = useState("");
+    const[Cidade,setCidade] = useState("");
+    const[DataNas, setDataNas] = useState("");
+    const[eCivil,seteCivil] = useState("");
+    const[DataCad,setDataCad] = useState("");
+    const[Raca,setRaca] = useState("");
+    const[PIS,setPis] = useState("");
+    const[Escolaridade,setEscolaridade] = useState("");
+    const[nEscola, setnEscola] = useState("");
+    const[dataEscola,setdataEscola] = useState("")
+    const[CTPS,setCTPS] = useState("");
+    const[CTPSSerie,setCTPSSeria] = useState("");
+    const[CTPSUF,setCTPSUF] = useState("");
+    const[CTPSdata,setCTPSdata] = useState("");
+    const[CMilitar,setCMilitar] = useState("");
+    const[CMilitarSerie,setCMilitarSerie] = useState("");
+    const[CMilitarCategoria] = useState("");
+    const[nPai,setnPai] = useState("");
+    const[Naturalidade,setNaturalidade] = useState("");
+    const[NatuUF,setNatuUF] = useState("");
+    const[Nacionalidade,setNacionalidade] = useState("");
+    const[SalarioHora,setSalarioHroa] = useState("");
+    const[Funcao,setFuncao] = useState("");
+    const[dataAdm,setdataAdm] = useState("");
+    const[horasSEM,sethorasSEM] = useState("");
+    const[horasAlmoco,sethorasAlmoco] = useState("");
+    const[TEleitor,setTEleitor] = useState("");
+    const[TEZona,setTEZona] = useState("");
+    const[TESecao,setTESecao] = useState("");
+    const[TEleitorData,setTEleitorData] = useState("");
+    const[nConjugue,setnConjugue] = useState("");
+    const[nDependentes,setnDependentes] = useState("");
+    const[nFilhos,setnFilhos] = useState("");*/
+    // ^Todas AS variaveis de empregação basica
+
+    const[ModalIsOpen, setModalIsOpen] = useState(false);
+
+    const handleOpenModal = () =>
+    {
+        setModalIsOpen(true);
+    }
+    const handleCloseModal = () => {
+        setModalIsOpen(false);
+    }
+
     return(
+
         <div className='content-box'>
+            <div className='button-box'>
+                <button onClick={handleOpenModal}>Add</button>
+                <ReactModal isOpen={ModalIsOpen} onRequestClose={handleCloseModal}>
+                    <CreateFuncionario />
+                </ReactModal>
+            </div>
             <div>
-                    <table>
-                        <thead>
-                            <tr>
-                            <th>Nome</th>
-                            <th>Email</th>
-                            <th>CPF</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td><button>Nome</button></td>
-                                <td><button>Email</button></td>
-                                <td><button>CPF</button></td>
-                            </tr>
-                            </tbody>
-                    </table>
-                </div>
+                <table>
+                    <thead>
+                        <tr>
+                        <th>Nome</th>
+                        <th>Email</th>
+                        <th>CPF</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td><button>Nome</button></td>
+                            <td><button>Email</button></td>
+                            <td><button>CPF</button></td>
+                        </tr>
+                        </tbody>
+                </table>
+            </div>
         </div>
     );
 }
