d3c2cfaf7e1c4976acece254c063b4f4b71ae825
Henrique Neves Lefkum <88463730+HenriqueLefkum@users.noreply.github.com>
2023-06-05 00:29:21 -0300

    Update firebaseConfig.js

--- src/config/firebaseConfig/firebaseConfig.js
+++ src/config/firebaseConfig/firebaseConfig.js
@@ -3,13 +3,7 @@ import {getFirestore} from 'firebase/firestore'
 
 //puxa o banco de dados
 const firebaseApp = initializeApp({
-    apiKey: "AIzaSyCtmACpdp5WRYiRo81ZTUQeAslNc6R3pPk",
-    authDomain: "siterh-ad136.firebaseapp.com",
-    projectId: "siterh-ad136",
-    storageBucket: "siterh-ad136.appspot.com",
-    messagingSenderId: "319601638665",
-    appId: "1:319601638665:web:3cdeb0f0aedd58a179e7d8",
-    measurementId: "G-9590PMJBLR"
+    //firebase Credentials
 });
 //inicia o banco de dados
 const db = getFirestore(firebaseApp);
